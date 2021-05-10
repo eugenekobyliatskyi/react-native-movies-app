4761a6245bf113420b4f7624f90e199a254df8db
Eugene Kobyliatskyi <eugenekobyliatskyi@gmail.com>
2021-05-10 15:19:56 +0300

    Add Search component

---
+++ src/components/Search.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, View, TextInput, Dimensions} from 'react-native';
+
+const Search = ({}) => {
+  return (
+    <View style={styles.wrapper}>
+      <TextInput style={styles.input} placeholder="Search" />
+    </View>
+  );
+};
+
+const width = Dimensions.get('window').width;
+const toNormalSize = n => (width * n) / 720;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: 56,
+    backgroundColor: 'white',
+    paddingHorizontal: toNormalSize(30),
+    paddingVertical: 10,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  },
+  input: {
+    borderBottomColor: '#808FA5',
+    borderBottomWidth: 1,
+    padding: 3,
+  },
+});
+
+export default Search;
